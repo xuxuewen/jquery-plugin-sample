1b7311fa2ac02ea79b290fc40ca40a90afbb4eb4
许学文 <xuxuewen@kpkj.org>
2018-01-03 00:18:52 +0800

    add docs

--- src/index.js
+++ src/index.js
@@ -1,23 +1,38 @@
-;(function($){
-    $.fn.helloWorld = function(options){
-        var settings = $.extend({
-          text:'Hello World',
-          color:null,
-          fontStyle:null,
-          complete:null
-        },options);
-        console.log(settings)
-        return this.each(function(){
-            $(this).text(settings.text);
-            if(settings.color){
-              $(this).css( 'color', settings.color );
-            }
-            if(settings.fontStyle){
-              $(this).css( 'font-style', settings.fontStyle );
-            }
-            if ( $.isFunction( settings.complete ) ) {
-              settings.complete.call( this );
-            }
-        })
-    }
-})(jQuery);
+/**
+ * @desc 这是一个用来修改dom的jquery插件
+ * @example
+ * $('div').helloWorld({
+ *  text:'修改的内容',
+ *  color:'red',
+ *  fontStyle:'blod',
+ *  complete:function(){alert('hello')}
+ * })
+ * @version 1.0.1
+ * @todo 测试用例未完成
+ * @return {jquery}         jquery dom 对象
+ */
+function helloWorldPlugin($) {
+  return $.fn.helloWorld = function(options) {
+    var settings = $.extend({
+      text: 'Hello World',
+      color: null,
+      fontStyle: null,
+      complete: null
+    }, options);
+    return this.each(function() {
+      $(this).text(settings.text);
+      if (settings.color) {
+        $(this).css('color', settings.color);
+      }
+      if (settings.fontStyle) {
+        $(this).css('font-style', settings.fontStyle);
+      }
+      if ($.isFunction(settings.complete)) {
+        settings.complete.call(this);
+      }
+    })
+  }
+}
+
+helloWorldPlugin(jQuery);
+export default helloWorldPlugin;
